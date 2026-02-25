7b845ea5fba13b035dccbfef5d9c6870ff8e560a
Farhan Tanvir <farhan.tanvir@craftsmensoftware.com>
2026-02-25 11:57:43 +0600

    add input validation and set types for ide type-hinting

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import argparse
+from datetime import datetime
+from datetime import date
+
+def parse_date(date:str) -> date:
+    try:
+        return datetime.strptime(date, "%Y-%m-%d").date()
+    except ValueError:
+        raise argparse.ArgumentTypeError("Invalid date format. Expected: YYYY-MM-DD")
 
 def main():
     parser = argparse.ArgumentParser(description="Simple CLI tool to show general fun facts based on birthday.")
-    parser.add_argument("-b", "--birthday", required=True, type=str, help="Your birthday in the format YYYY-MM-DD")
+    parser.add_argument("-b", "--birthday", required=True, type=parse_date, help="Your birthday in the format YYYY-MM-DD")
     args = parser.parse_args()
-    if args.birthday:
-        print(f"Your birthday is: {args.birthday}")
+    
+    birthday:date = args.birthday
+    print(f"Your birthday is: {birthday}")
+
 
 if __name__ == "__main__":
     main()
